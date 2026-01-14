14345ff4985d925982d5fb09756360bd2979b586
Kelly <kellywwk4@gmail.com>
2026-01-14 17:36:11 +1100

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+//MARK: Shell loop
+//Read, Parse, Execute
+void lsh_loop(void) {
+    
+}
+
+//MARK: Main 
+// initialze, interpret, terminate
+int main (int argc, char **argv) {
+    //load files if any
+
+    //run command loop
+    lsh_loop();
+
+    //terminate / perform shutdown/cleanup
+
+    return 0;
+}
