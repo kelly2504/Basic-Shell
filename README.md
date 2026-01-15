eecd00caabf252452764fc4f99db86f759c2f215
Kelly <kellywwk4@gmail.com>
2026-01-15 21:00:13 +1100

    Feature: Making Launch function + ReadMe

---
+++ README.md
@@ -0,0 +1,14 @@
+## Basic Shell Implementation in C
+This project was made under the tutorial of Stephen Brennan. Big Shoutout to him. 
+This was an interesting project to do. Mostly because I had no idea what a shell was
+even though I have used bash in the past and WSL.
+
+Now, the first mistake I made is that this project was created on Windows. Yes.
+Despite my attempt at trying to teach myself how to be a software engineer I messed up
+at step number 1. So if you so happen to read this, please do not be an idiot like I was,
+and make the project right off the bat on Ubuntu. While the git history does show that I
+have in fact completed this project on the same project folder, I did have to migrate my files
+to WSL to make testing possible. Will I publish the other project to GitHub? I don't know, maybe?
+
+Anyways conclusion is do your research first and do not be an idiot if you're on Windows.
+(If you're reading this from a Macbook fuck you...)
--- src/main.c
+++ src/main.c
@@ -2,11 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/utime.h>
+
 
 //MARK: defintions
 #define LSH_RL_BUFSIZE 1024
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
+
+//MARK: Launching a program
+//parameter takes a list of pointer arguments
+int lsh_launch(char **args) {
+    //pid_t data type refers to process identification
+    //it is a signed integer type, int in the GNU C library
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        //Child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        //Error forking
+        perror("lsh");
+    } else {
+        //Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIDEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
 
 
 //MARK: READ A LINE
